3669c174f905bf385b39a190c71da7d6ea34741d
Wael Ashraf <wael.ashraf.anwar@gmail.com>
2019-04-17 04:25:45 +0200

    Added CORs in server.js

--- server.js
+++ server.js
@@ -19,8 +19,28 @@ const Books = require("./routes/Books");
 const OwnedBooks= require("./routes/ownedBooks");
 
 const app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-app.use(express.static('static'));
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static('static'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 
 //Haitham -- cause error please check it 
 /* if (!config.get('jwtPrivateKey')) {
