baade921e4ca72d4db98df1f27a372557018969c
Wael Ashraf <wael.ashraf.anwar@gmail.com>
2019-04-28 17:57:11 +0200

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');  //receives Token From User
+  //const token = req.header('x-auth-token');  //receives Token From User
+  const token = req.params.token;
+  
   if (!token) return res.status(401).send({"ReturnMsg":'Access denied. No token provided.'});//Makes Sure A token is Sent
 
   try {
